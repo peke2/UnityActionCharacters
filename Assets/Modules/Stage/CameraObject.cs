846e192aa1dab93e40bdc6131ed9a3f7802f1b65
peke2 <abem@dc4.so-net.ne.jp>
2019-01-22 01:34:28 +0900

    カメラのプレイヤー追尾を修正。 座標補正が間違っていたので正しく追尾できていなかったため。

--- Assets/Modules/Stage/CameraObject.cs
+++ Assets/Modules/Stage/CameraObject.cs
@@ -13,17 +13,15 @@ namespace Stage
 
 		public void initCameraInfo(float x, float y, float w, float h, float areaW, float areaH)
 		{
-			m_position = new Vector2(x, y);
 			m_size = new Vector2(w, h);
 			m_scopeSize = m_size * 0.7f;
 			m_stageSize = new Vector2(areaW, areaH);
-			limit();
+			m_position = limit(new Vector2(x, y));
 		}
 
 		public void setPosition(float x, float y)
 		{
-			m_position = new Vector2(x, y);
-			limit();
+			m_position = limit(new Vector2(x, y));
 		}
 
 		public void update()
@@ -47,12 +45,20 @@ namespace Stage
 			var halfScope = m_scopeSize / 2;
 			var adjust = Vector2.zero;
 
-			if( (diff.x < -halfScope.x) || (diff.x > halfScope.x) )
+			if( diff.x < -halfScope.x )
+			{
+				adjust.x = diff.x + halfScope.x;
+			}
+			else if( diff.x > halfScope.x )
 			{
 				adjust.x = diff.x - halfScope.x;
 			}
 
-			if((diff.y < -halfScope.y) || (diff.y > halfScope.y))
+			if( diff.y < -halfScope.y )
+			{
+				adjust.y = diff.y + halfScope.y;
+			}
+			else if( diff.y > halfScope.y )
 			{
 				adjust.y = diff.y - halfScope.y;
 			}
@@ -63,12 +69,12 @@ namespace Stage
 			cameraObj.transform.position = cameraPos;
 		}
 
-		void limit()
+		Vector2 limit(Vector2 pos)
 		{
 			//	左下が原点(0,0)の座標系
 			var halfSize = m_size / 2;
-			var lb = m_position - halfSize;
-			var rt = m_position + halfSize;
+			var lb = pos - halfSize;
+			var rt = pos + halfSize;
 
 			var areaLb = new Vector2(0, 0);
 			var areaRt = m_stageSize;
@@ -77,30 +83,32 @@ namespace Stage
 			diff = lb.x - areaLb.x;
 			if(diff < 0)
 			{
-				m_position.x += diff;
+				pos.x -= diff;
 			}
 			else
 			{
-				diff = areaRt.x - rt.x;
-				if(diff < 0)
+				diff = rt.x - areaRt.x;
+				if(diff > 0)
 				{
-					m_position.x += diff;
+					pos.x -= diff;
 				}
 			}
 
 			diff = lb.y - areaLb.y;
 			if(diff < 0)
 			{
-				m_position.y += diff;
+				pos.y -= diff;
 			}
 			else
 			{
-				diff = areaRt.y - rt.y;
-				if(diff < 0)
+				diff = rt.y - areaRt.y;
+				if(diff > 0)
 				{
-					m_position.y += diff;
+					pos.y -= diff;
 				}
 			}
+
+			return pos;
 		}
 
 		// Use this for initialization
